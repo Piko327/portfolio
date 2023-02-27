c288ae72167adf16ad3ea8e375f6c29dd6a7272f
Piko327 <pikopikop372@gmail.com>
2023-02-27 18:23:52 +0200

    Home css

--- src/Coponents/Home/Home.js
+++ src/Coponents/Home/Home.js
@@ -3,9 +3,9 @@ export const  Home =()=>
 {
    return <div className="home ">
         <h1 className='home_title'>Hi, I'm Fiki</h1>
-        <p  className='home_decription'>front-end developer</p>
        <img className="home_img" src={picture}  alt="profile" />
-        <button>
+       <p  className='home_decription'>front-end developer</p>
+        <button className="home_btn">
             Download CV
         </button>
     </div>
