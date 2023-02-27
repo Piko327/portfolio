cba9f671bf1af043ec5162a8f5731260e2e4d5a7
Piko327 <pikopikop372@gmail.com>
2023-02-27 11:44:24 +0200

    Structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  );
+  return ( 
+    <>
+    {/* <Header/>
+      <Home/>
+      <About/>
+      <Project/>
+       */}
+       
+    </>
+  )
 }
 
 export default App;
