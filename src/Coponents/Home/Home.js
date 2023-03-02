23121d61ffd33919468433502b0b09c8684f0895
Piko327 <pikopikop372@gmail.com>
2023-03-02 19:47:55 +0200

    add flexbox in home

--- src/Coponents/Home/Home.js
+++ src/Coponents/Home/Home.js
@@ -1,10 +1,11 @@
 import  picture from  "./Subject.png"
 export const  Home =()=>
 {
-   return <div className="home active">
+   return <div className="home">
         <h1 className='home_title'>Hi, I'm Fiki</h1>
        <img className="home_img" src={picture}  alt="profile" />
-       <p  className='home_decription'>front-end developer</p>
+       <h2  className='home_second_title'>front-end developer</h2>
+       <p className="home_description">i craft user interfaces and aplications using modern frontend technologies</p>
         <button className="home_btn">
             Download CV
         </button>
