704f9adae561084a490f0ce7375fe5d53a50d01f
Piko327 <pikopikop372@gmail.com>
2023-04-12 20:36:09 +0300

    starting with about page

--- src/Coponents/About/About.js
+++ src/Coponents/About/About.js
@@ -3,6 +3,23 @@ import React from 'react'
 const About = () => {
   return (
      <>
+     <section className='education_section'>
+   <div className='slide-content'>
+   <div className='slide-contnet'>
+    <div className='card-wrapper'>
+      <div className='card'>
+        <div className='image-content'></div>
+        <span className='overlay'></span>
+        <div className='card-image'>
+          <embed src="certificates\Java Advanced - January 2022 - Certificate.pdf"></embed>
+        </div>
+      </div>
+    </div>
+   </div>
+   </div>
+
+
+     </section>
 <section className='skills_section'>
 <h1 className='skills_title'>
     SKILLS
