bc7acf944f871725ba35a0dbd31231de5b23f97a
Piko327 <pikopikop372@gmail.com>
2023-04-12 19:11:51 +0300

    home css

--- src/Coponents/About/About.js
+++ src/Coponents/About/About.js
@@ -4,10 +4,10 @@ const About = () => {
   return (
      <>
 <section className='skills_section'>
-<h1>
+<h1 className='skills_title'>
     SKILLS
 </h1>
-    <ul className='skils'>
+    <ul className='skills'>
         <li className='skill'>HTML</li>
         <li className='skill'>CSS</li>
         <li className='skill'>JS</li>
@@ -17,22 +17,7 @@ const About = () => {
         <li className='skill'>TailWind</li>
         <li className='skill'>Firebase</li>
     </ul>
-
-
 </section>
-
-<selection className="education">
- <h1 className='educations_title'></h1>
-  <ul className='education_ul'>
-    <li className='education_li'>
-   <article>
-   <img className="home_img" src={}alt="profile" />
-   <h2 className='education_title'></h2>
-      <p className='education_description'></p>
-      </article>
-    </li>
-  </ul>
-</selection>
   </> 
   )
 }
--- src/Coponents/Home/Home.js
+++ src/Coponents/Home/Home.js
@@ -2,9 +2,14 @@ import  picture from  "./Subject.png"
 export const  Home =()=>
 {
    return <div className="home">
-        <h1 className='home_title'>Hey, I'm FiKi</h1>
+       <div className='home_title' >
+        <p className="home_title_one">Hello</p> 
+        <p>, I'm Fiki </p>
+        </div>
        <img className="home_img" src={picture}  alt="profile" />
-       <h2  className='home_second_title'>front-end developer</h2>
+     <div className='home_second_title'>
+     <p className="home_second_title_one">front-end </p><p className='home_second_title_second'>developer</p>
+     </div>
        <p className="home_description"> A passionate developer having an expirience  of bulding Web Sites and Aplications with JavaScript / React / CSS / HTML 
        </p>
         <button className="home_btn">
