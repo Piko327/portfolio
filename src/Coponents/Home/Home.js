7e0ab1bfab48db2543cd152da8320a8fe45c037b
Piko327 <pikopikop372@gmail.com>
2023-04-23 23:47:53 +0300

    home page done

--- src/Coponents/Home/Home.js
+++ src/Coponents/Home/Home.js
@@ -2,19 +2,28 @@ import  picture from  "./Subject.png"
 export const  Home =()=>
 {
    return <div className="home">
+      <div className="part">
        <div className='home_title' >
         <p className="home_title_one">Hello</p> 
         <p>, I'm Fiki </p>
         </div>
        <img className="home_img" src={picture}  alt="profile" />
+       </div>
+       <div className="part">
      <div className='home_second_title'>
-     <p className="home_second_title_one">front-end </p><p className='home_second_title_second'>developer</p>
+     <p className="home_second_title_one">front-end </p>
+     <p className='home_second_title_second'>developer</p>
      </div>
-       <p className="home_description"> A passionate developer having an expirience  of bulding Web Sites and Aplications with JavaScript / React / CSS / HTML 
-       </p>
-        <button className="home_btn">
+    
+
+       <span className="home_description"> 
+       A passionate developer having an expirience
+         of bulding Web Sites and Aplications with JavaScript 
+         / React / CSS / HTML 
+       </span>
+        <button className="home_btn ">
             Download CV
         </button>
-        
+        </div>
     </div>
 }
