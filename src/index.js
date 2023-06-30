2a28c1806b813c178cbaf9eb5fef9aa3f9c29938
Piko327 <pikopikop372@gmail.com>
2023-06-30 10:46:30 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import  './Coponents/Home/Home.css'
-import  './Coponents/Header/Header.css'
-import "./Coponents/About/Education/Education.css"
-import "./Coponents/About/Skills/Skills.css"
-import "./Coponents/Project/Projects.css"
-import './Coponents/Contact/Contact.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />
-
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./Coponents/Home/Home.css";
+import "./Coponents/Header/Header.css";
+import "./Coponents/About/Education/Education.css";
+import "./Coponents/About/Skills/Skills.css";
+import "./Coponents/Project/Projects.css";
+import "./Coponents/Contact/Contact.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
