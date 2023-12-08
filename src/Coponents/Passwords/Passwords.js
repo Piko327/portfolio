034928d522e097d04e168ea4d4b13bfdc3a73b0a
Piko327 <fikii.business@gmail.com>
2023-12-08 13:13:28 +0200

    add condition to render after auth and Icons

--- src/Coponents/Passwords/Passwords.js
+++ src/Coponents/Passwords/Passwords.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { CgMail } from "react-icons/cg";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
+import { CiFacebook } from "react-icons/ci"
+
+
 const Passwords = () => {
 
     const [user, setUser] = useState(null);
@@ -26,21 +30,43 @@ const Passwords = () => {
     };
     
     return (
-      
+      <div>
+        {user?.uid == null ? (
+          <div className="passwords">
+            <h2>Please first authenticate !!</h2>
 
-        //condition to show //paswords
-    <div className="passwords">
-      <h2>Please first authenticate !!</h2>
+            <button className="btn" onClick={sendOtp}>
+              Send Code
+            </button>
 
-      <button className="btn" onClick={sendOtp}>
-        Send Code
-      </button>
+            <div id="recaptcha"></div>
+            <input onChange={(e) => setOtp(e.target.value)} type="text"></input>
+            <button onClick={() => verifyOtp()} className="btn">
+              Submit
+            </button>
+          </div>
+        ) : (
+          <ul>
+            <li>
+              <CiFacebook />
+            </li>
+            <li>
+              <CgMail />
+            </li>
 
-      <div id="recaptcha"></div>
-          <input onChange={(e) =>setOtp(e.target.value )} type="text"></input>
-      <button  onClick={()=>verifyOtp()} className="btn">Submit</button>
-    </div>
-  );
+            <li>
+              <CgMail />
+            </li>
+
+            <li></li>
+
+            <li></li>
+
+            <li></li>
+          </ul>
+        )}
+      </div>
+    );
 };
 
 export default Passwords;
