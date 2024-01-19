d96a0ef285f13b59d456d4095e0224b772658eb1
Piko327 <fikii.business@gmail.com>
2024-01-19 16:49:58 +0200

    add animation with framer-motion

--- src/Coponents/About/Education/Education.js
+++ src/Coponents/About/Education/Education.js
@@ -7,6 +7,8 @@ import Card from "./Card";
 import "swiper/css/effect-coverflow";
 import useWindowDimensions from "../../../hooks/useWindowDimensions";
 import { RiBookletLine } from "react-icons/ri";
+import { motion } from "framer-motion";
+import ShowEffectProvider from "../../../helpers/ShowEffect";
 
 const Education = () => {
     const { width } = useWindowDimensions();
@@ -30,100 +32,104 @@ const Education = () => {
     }, [width]);
 
     return (
-        <section className="education">
-            <div className="slide-container">
-                <h1 className="education_title">
-                    <RiBookletLine className="eudcation_icon" /> education
-                </h1>
+        <ShowEffectProvider >
+            <section className="education">
+                <div className="slide-container">
+                    <h1 className="education_title">
+                        <RiBookletLine className="eudcation_icon" /> education
+                    </h1>
 
+                    <div className="education_desc">
+                        <p>
+                            During my journey as a developer, I initially gained a solid
+                            foundation of Java programming. Through Java Fundamentals and Java
+                            Advanced courses, I learned about object-oriented programming (OOP)
+                            principles, SOLID principles, and more, which enabled me to design
+                            to build robust software systems. Moving into web development, I
+                            immersed myself in the JavaScript. The Advanced JavaScript course
+                            further enhanced my skills by exploring key concepts such as
+                            Functions, arrays, and objects. I honed my ability to write clean
+                            and efficient code, which enabled me to create dynamic and
+                            interactive web experience.
+                        </p>
+                    </div>
+
+                    <Swiper
+                        className="swiper"
+                        effect={"coverflow"}
+                        grabCursor={true}
+                        navigation
+                        centeredSlides={true}
+                        slidesPerView={slidesPerViewCount}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 10,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: false,
+                        }}
+                        pagination={true}
+                        modules={[EffectCoverflow, Pagination, Navigation]}
+                    >
+                        <SwiperSlide>
+                            <Card
+                                img="/certificates/JS Advanced - September 2022 - Certificate.jpeg"
+                                title={"JS Advanced "}
+                            />{" "}
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <Card
+                                img="/certificates/JS Applications - October 2022 - Certificate.jpeg"
+                                title={"JS Applications "}
+                            />{" "}
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <Card
+                                img="/certificates/Java Advanced - January 2022 - Certificate.jpeg"
+                                title={"Java Advanced  "}
+                            />{" "}
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <Card
+                                img="/certificates/Java OOP - February 2022 - Certificate.jpeg"
+                                title={"Java OOP "}
+                            />{" "}
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <Card
+                                img="/certificates/Programming Fundamentals with Java - September 2021 - Certificate.jpeg"
+                                title={"Java Fundamentals"}
+                            />{" "}
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <Card
+                                img="/certificates/MySQL - September 2022 - Certificate.jpeg"
+                                title={"MySQL"}
+                            />{" "}
+                        </SwiperSlide>
+                        ...
+                    </Swiper>
+                </div>
                 <div className="education_desc">
                     <p>
-                        During my journey as a developer, I initially gained a solid
-                        foundation of Java programming. Through Java Fundamentals and Java
-                        Advanced courses, I learned about object-oriented programming (OOP)
-                        principles, SOLID principles, and more, which enabled me to design
-                        to build robust software systems. Moving into web development, I
-                        immersed myself in the JavaScript. The Advanced JavaScript course
-                        further enhanced my skills by exploring key concepts such as
-                        Functions, arrays, and objects. I honed my ability to write clean
-                        and efficient code, which enabled me to create dynamic and
-                        interactive web experience.
+                        I improved my JavaScript skills through the practical JS Application
+                        course, mastering HTTP requests, REST API, and authentication. With a
+                        deeper understanding of JavaScript app architecture, I can design
+                        responsive web application. I know web technologies, I create robust
+                        and engaging web applications by leveraging the fundamental concepts
+                        of JavaScript and the application architecture. My solutions are
+                        efficient and dynamic.
                     </p>
                 </div>
+            </section>
+        </ShowEffectProvider>
 
-                <Swiper
-                    className="swiper"
-                    effect={"coverflow"}
-                    grabCursor={true}
-                    navigation
-                    centeredSlides={true}
-                    slidesPerView={slidesPerViewCount}
-                    coverflowEffect={{
-                        rotate: 50,
-                        stretch: 10,
-                        depth: 100,
-                        modifier: 1,
-                        slideShadows: false,
-                    }}
-                    pagination={true}
-                    modules={[EffectCoverflow, Pagination, Navigation]}
-                >
-                    <SwiperSlide>
-                        <Card
-                            img="/certificates/JS Advanced - September 2022 - Certificate.jpeg"
-                            title={"JS Advanced "}
-                        />{" "}
-                    </SwiperSlide>
 
-                    <SwiperSlide>
-                        <Card
-                            img="/certificates/JS Applications - October 2022 - Certificate.jpeg"
-                            title={"JS Applications "}
-                        />{" "}
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Card
-                            img="/certificates/Java Advanced - January 2022 - Certificate.jpeg"
-                            title={"Java Advanced  "}
-                        />{" "}
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Card
-                            img="/certificates/Java OOP - February 2022 - Certificate.jpeg"
-                            title={"Java OOP "}
-                        />{" "}
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <Card
-                            img="/certificates/Programming Fundamentals with Java - September 2021 - Certificate.jpeg"
-                            title={"Java Fundamentals"}
-                        />{" "}
-                    </SwiperSlide>
-                    
-                    <SwiperSlide>
-                        <Card
-                            img="/certificates/MySQL - September 2022 - Certificate.jpeg"
-                            title={"MySQL"}
-                        />{" "}
-                    </SwiperSlide>
-                    ...
-                </Swiper>
-            </div>
-            <div className="education_desc">
-                <p>
-                    I improved my JavaScript skills through the practical JS Application
-                    course, mastering HTTP requests, REST API, and authentication. With a
-                    deeper understanding of JavaScript app architecture, I can design
-                    responsive web application. I know web technologies, I create robust
-                    and engaging web applications by leveraging the fundamental concepts
-                    of JavaScript and the application architecture. My solutions are
-                    efficient and dynamic.
-                </p>
-            </div>
-        </section>
     );
 };
 
